3ae501036f27ae94d22a9e5d66266ee67ffd5fd7
Nikolas Mouzourides <nikolas.mouzourides@blackpepper.co.uk>
2017-10-20 12:28:02 +0100

    Added skeleton code for tests

---
+++ src/test/java/SportSimulatorTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SportSimulatorTest {
+    @Before
+    public void setUp(){
+        SportSimulator sportSimulator = new SportSimulator();
+    }
+
+    @Test
+    public void testSports(){
+
+    }
+
+}
